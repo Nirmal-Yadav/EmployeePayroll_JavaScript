63f7f426e7a8f969242df3a49bdeba48a2e5f23f
Nirmal Yadav <yadavnirmal07@gmail.com>
2021-10-06 20:22:23 +0530

    added: calculated employee wage

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,6 +1,21 @@
-const IS_ABSENT=0
-let empCheck =Math.floor(Math.random()* 10) %2;
-if (empCheck == IS_ABSENT)
-console.log("absent")
-else
-console.log("present");
+
+const IS_FULL_TIME=2;
+const IS_PART_TIME=1;
+const PART_TIME_HOURS=4;
+const FULL_TIME_HOURS=8;
+const WAGE_PER_HOURS=20;
+
+empHrs=0;
+let empCheck =Math.floor(Math.random()* 10) %3;
+switch(empCheck){
+    case IS_PART_TIME:
+        empHrs=PART_TIME_HOURS;
+        break;
+    case IS_FULL_TIME:
+        empHrs=8;
+        break;
+    default:
+       empHrs=0;
+}
+let empWage=empHrs*WAGE_PER_HOURS;
+console.log("employee Wage = " +empWage)
