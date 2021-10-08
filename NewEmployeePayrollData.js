aa09b0e87ce775cbfefe55b432d69e8074b0cc63
Nirmal Yadav <yadavnirmal07@gmail.com>
2021-10-08 20:32:23 +0530

    added: gender nd startdate

--- NewEmployeePayrollData.js
+++ NewEmployeePayrollData.js
@@ -2,13 +2,16 @@ class EmployeePayrollData {
     id;
     salary;
     gender;
-    startdate;
+    startDate;
 
-    constructor(id,name,salary){
-        this.id=id;
-        this.name=name;
-        this.salary=salary;
-    }
+   constructor(...params){
+       this.id=params[0]
+       this.name=params[1]
+       this.salary=params[2]
+       this.gender=params[3]
+       this.startDate=params[4]
+
+   }
     get name(){
         return this._name
     }
@@ -17,13 +20,16 @@ class EmployeePayrollData {
         this._name=name;
     }
     toString(){
-        return "id= "+this.id +", name = " +this.name + ", salary = " +this.salary;
+        return "id= "+this.id +", name = " +this.name + ", salary = " +this.salary 
+        +", gender = " +this.gender + ", startDate = " +this.startDate;
     }
 }
 let employeePayrollData=new EmployeePayrollData(1,"mark",30000);
 console.log(employeePayrollData.toString());
-//employeePayrollData.name="john";
+employeePayrollData.name="john";
 
 employeePayrollData.id=0
 employeePayrollData.name="jeff"
 console.log(employeePayrollData.toString());
+let newEmployeePayrollData=new EmployeePayrollData(1,"terrisa",30000,"f",new Date());
+console.log(newEmployeePayrollData.toString());
